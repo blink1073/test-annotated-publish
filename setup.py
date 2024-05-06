60929ff7e7757ed0f71bcca5c982d28fb514fd18
Steven Silvester <steve.silvester@mongodb.com>
2024-05-06 06:06:25 -0500

    Add wheel building

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
 from setuptools import setup
-setup()
+try:
+    from Cython.Build import cythonize
+    ext_modules = cythonize("test_annotated_publish/helloworld.pyx")
+except ImportError:
+    ext_modules=[]
+
+setup(
+    ext_modules = ext_modules
+)
--- test_annotated_publish/__init__.py
+++ test_annotated_publish/__init__.py
@@ -1,2 +1,4 @@
+
+from test_annotated_publish import helloworld
 def example_function():
     return 1 + 1
